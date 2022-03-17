ced233a794091347ef606a7d1232590cf3145431
NDALER <sdalvianto@protonmail.com>
2022-03-17 13:05:47 +0800

    nothin really important in this patch

--- config.h
+++ config.h
@@ -9,7 +9,11 @@ static const unsigned int gappoh    = 7;        /* horiz outer gap between windo
 static const unsigned int gappov    = 7;        /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+<<<<<<< HEAD
 static const int attachdirection    = 3;        /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
+=======
+static const int attachdirection    = 3;    /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
+>>>>>>> fe15bbb (nothin really important in this patch)
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"Firacode nerd font mono:pixelsize=13.5:antialias=true:autohint=true", "Noto Sans JP:size=12", };
